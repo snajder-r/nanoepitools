d0ff6f77218eb1cabd58b50990d9d1dc2de80ff9
Rene Snajder <snajder@stud.uni-heidelberg.de>
2020-08-21 12:54:32 +0200

    Added multi-page pdf plotting to the plotarchiver

--- nanoepitools/plotting/general_plotting.py
+++ nanoepitools/plotting/general_plotting.py
@@ -1,12 +1,26 @@
 from pathlib import Path
 
 import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
 from bokeh.plotting import figure, output_file
 
 
 def set_if_not_in_dict(d, k, v):
     if k not in d.keys():
         d[k] = v
+
+
+class PDFPagesWrapper:
+    def __init__(self, pa, *argv):
+        self.pa = pa
+        self.pdf = PdfPages(*argv)
+
+    def __enter__(self):
+        self.pdf.__enter__()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.pa.pdf = None
+        self.pdf.__exit__(exc_type, exc_val, exc_tb)
 
 
 class PlotArchiver:
@@ -21,6 +35,7 @@ class PlotArchiver:
 
         self.project_path = Path(self.config['plot_archive_dir']).joinpath(
             self.project)
+        self.pdf: PDFPagesWrapper = None
 
     def ensure_project_path_exists(self):
         self.project_path.mkdir(parents=True, exist_ok=True)
@@ -31,20 +46,33 @@ class PlotArchiver:
         filename = '{key}.{ft}'.format(key=key, ft=filetype)
         return self.project_path.joinpath(filename)
 
-    def savefig(self, key, fig=None):
+    def savefig(self, key='figure', fig=None, close=True):
         self.ensure_project_path_exists()
         if fig is None:
             fig = plt.gcf()
-        path = self.get_plot_path(key)
-        fig.savefig(path)
+        if self.pdf is not None:
+            self.pdf.pdf.savefig(fig)
+        else:
+            path = self.get_plot_path(key)
+            fig.savefig(path)
+        if close:
+            plt.close(fig)
 
     def saveandshow(self, key, fig=None):
         if fig is None:
             fig = plt.gcf()
-        self.savefig(key, fig=fig)
+        self.savefig(key, fig=fig, close=False)
         fig.show()
+        plt.close(fig)
 
     def bokeh_open_html(self, key):
         self.ensure_project_path_exists()
         path = self.get_plot_path(key, 'html')
         output_file(path)
+
+    def open_multipage_pdf(self, key):
+        self.ensure_project_path_exists()
+        path = self.get_plot_path(key, 'pdf')
+        self.pdf = PDFPagesWrapper(self, path)
+        return self.pdf
+

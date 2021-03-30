4baa40023c2495122ed461f1bd4dd2452a7d18ce
snajder <rene.snajder@gmail.com>
2021-03-30 14:34:36 +0200

    Now returning every match, including lnc_rna and created a searcher class that searches in linear time if input is sorted

--- nanoepitools/annotations.py
+++ nanoepitools/annotations.py
@@ -1,9 +1,10 @@
 import re
 from typing import Union, IO
-
 from pathlib import Path
-import pandas as pd
 import itertools
+
+import numpy as np
+import pandas as pd
 
 
 def range_comparison_dist_fn(x, start, end):
@@ -13,7 +14,7 @@ def range_comparison_dist_fn(x, start, end):
     else:
         # Overlapping:
         return 0
-    
+
 def point_to_promoter_dist_fn(feature, start, end, promoter_before_tss, promoter_after_tss):
     if feature.direction == "+":
         promoter_range = [feature.start - promoter_before_tss, feature.start + promoter_after_tss]
@@ -57,23 +58,53 @@ class GFFFeature:
         for child in self.sorted_children:
             child.build_sorted()
     
-    def get_in_range(self, start, end, max_recursion=0):
+    def get_in_range(self, start, end, range_transform = None, min_recursion=0, max_recursion=0):
+        if range_transform is None:
+            # identity
+            def range_transform(x):
+                return x.start, x.end
         for feature in self.sorted_children:
-            if feature.start > end:
+            if range_transform(feature)[0] > end and min_recursion <= 0:
                 break
-            elif feature.end < start:
+            elif range_transform(feature)[1] < start and min_recursion <= 0:
                 continue
             else:
-                yield feature
                 if max_recursion > 0:
-                    for x in feature.get_in_range(start, end, max_recursion-1):
+                    for x in feature.get_in_range(start, end, range_transform=range_transform, max_recursion=max_recursion-1, min_recursion=min_recursion-1):
                         yield x
+                else:
+                    yield feature
     
-
+    def get_sorted_in_range_finder(outer_self):
+        """If you are searching for annotations for a SORTED list of ranges,
+        then this allows you to do so in a near linear time, as it continues
+        to move up the pointer at which searching starts"""
+        class seeker:
+            def __init__(inner_self):
+                inner_self.earliest_index = 0
+            
+            def find(inner_self, start, end, range_transform=None, max_recursion=0, min_recursion=0):
+                if range_transform is None:
+                    # identity
+                    def range_transform(x):
+                        return x.start, x.end
+                
+                for feature in outer_self.sorted_children[inner_self.earliest_index:]:
+                    if range_transform(feature)[0] > end and min_recursion <= 0:
+                        break
+                    elif range_transform(feature)[1] < start and min_recursion <= 0:
+                        inner_self.earliest_index+=1
+                        continue
+                    else:
+                        if max_recursion > 0:
+                            for x in feature.get_in_range(start, end, range_transform=range_transform, max_recursion=max_recursion - 1, min_recursion=min_recursion-1):
+                                yield x
+        return seeker()
+    
     def get_nearest_feature(self, start=None, end=None, dist_fn=None, nearest=None, nearest_dist=10e15, max_recursion=0, min_recursion=0):
         if dist_fn is None:
             dist_fn = lambda x: range_comparison_dist_fn(x, start, end)
-            
+        
         for feature in self.sorted_children:
             if min_recursion > 0 and len(feature.sorted_children) > 0:
                 _, dist = feature.get_nearest_feature(dist_fn=dist_fn, max_recursion=max_recursion - 1, min_recursion=min_recursion-1)
@@ -89,7 +120,7 @@ class GFFFeature:
                 nearest = feature
             else:
                 continue
-                
+        
         if nearest is None:
             return None, np.inf
         
@@ -99,11 +130,11 @@ class GFFFeature:
             return nearest, nearest_dist
 
 class GFFAnnotationsReader:
-
+    
     def __init__(self):
         self.chromosomes: Dict[str, GFFFeature] = {}
-        self.included_features = ["gene", "mRNA", "exon"]
-
+        self.included_features = ["gene", "mRNA", "exon", "ncRNA_gene", "pseudogene", "lnc_RNA"]
+    
     def read(self, gff_file, only_protein_coding=True):
         with open(gff_file, "rt") as f:
             
@@ -111,56 +142,70 @@ class GFFAnnotationsReader:
                 {"chr": l[0], "type": l[2], "start": int(l[3]), "end": int(l[4]), "direction": l[6], "info": l[8]}
                 for l in (l.split("\t") for l in f if l[0] != "#")
             )
-
+            
             cur_chrom = None
             for row in rowit:
                 if cur_chrom is None or cur_chrom.id != row["chr"]:
                     cur_chrom = GFFFeature(0, 0, type="chrom", direction="+", id=row["chr"])
                     self.chromosomes[row["chr"]] = cur_chrom
                     open_features: Dict[str, GFFFeature] = {}
-
+                
                 if row["type"] not in self.included_features:
                     continue
-
+                
                 info = [kv.split("=") for kv in row["info"].split(";")]
                 info = {k: v for k, v in info}
                 id = info.get("ID", None)
                 name = info.get("Name", None)
                 parent_id = info.get("Parent", None)
-
+                
                 if "biotype" in info:
                     if only_protein_coding and not info["biotype"] == "protein_coding":
                         continue
-
+                elif only_protein_coding:
+                    # "only_protein_coding" requires a biotype annotation
+                    continue
+                
                 cur_feature = GFFFeature(row["start"], row["end"], type=row["type"], id=id, direction=row["direction"], name=name)
-
+                
                 if parent_id is not None:
                     if parent_id not in open_features:
                         continue
                     cur_parent = open_features[parent_id]
                 else:
                     cur_parent = cur_chrom
-
+                
                 cur_feature.parent = cur_parent
-
+                
                 if id is None:
                     # Only leafs have no id
                     cur_parent.leafs.append(cur_feature)
                 else:
                     cur_parent.children[id] = cur_feature
                     open_features[id] = cur_feature
-
+    
     def build_index(self):
         for chrom_container in self.chromosomes.values():
             chrom_container.build_sorted()
-
+    
+    def get_gene(self, gene_id: str) -> GFFFeature:
+        """
+        :param gene_id: Gene ID without the "gene:" part at the beginning
+        """
+        genes_found = [chrom.children[f"gene:{gene_id}"] for chrom in self.chromosomes.values() if f"gene:{gene_id}" in chrom.children]
+        assert len(genes_found) <=1, "Multiple genes with the same id were found"
+        if len(genes_found) == 0:
+            return None
+        else:
+            return genes_found[0]
+    
     def _print(self, feature, depth=2):
         for fid, f in feature.children.items():
             print("".join(["-"] * depth), fid)
             self._print(f, depth + 2)
         for f in feature.leafs:
             print("".join(["-"] * depth), f.start, f.end)
-
+    
     def print(self):
         for chromname, chrom in self.chromosomes.items():
             print(chromname)
